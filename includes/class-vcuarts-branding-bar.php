630094c5bd88a68de9e2380bba09cb4bfeced55b
Cody W <cody@codywhitby.net>
2016-01-13 15:20:10 -0500

    less generic classes

--- includes/class-vcuarts-branding-bar.php
+++ includes/class-vcuarts-branding-bar.php
@@ -58,7 +58,7 @@ class VCUarts_Branding_Bar {
 	public function __construct() {
 
 		$this->vcuarts_branding_bar = 'vcuarts-branding-bar';
-		$this->version = '1.0.0';
+		$this->version = '1.0.01';
 
 		$this->load_dependencies();
 		$this->define_admin_hooks();
